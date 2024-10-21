78729604d3837f64150b945b7f289436ce683156
Jair <jairaceves56@gmail.com>
2024-10-21 14:46:22 -0600

    Footer modificado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
   return (
     <div className="mt-6 bg-[#788d1c] text-center">
-      <div className='w-full p-2 pl-12 pr-12 flex justify-between'>
-        <p className="mt-2 text-white text-lg flex items-center">Aviso de Privacidad:</p>
-        <p className="mt-2 text-white text-lg flex items-center">Desarrollado por:
-          <img src="img/Binte-Logo.png" alt="logo empresa" className='w-24' />
+      <div className="w-full p-4 flex flex-col md:flex-row items-center justify-between">
+        <p className="text-white text-lg flex items-center mb-2 md:mb-0">
+          <a 
+            href="/factura-ERN/assets/pdf/Aviso-de-Privacidad-BINTE.pdf" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="underline hover:text-gray-200"
+            >
+            Aviso de Privacidad
+          </a>
+        </p>
+        <p className="text-white text-lg flex items-center">
+          Desarrollado por:
+          <a
+            href="https://binteconsulting.com/" 
+            target="_blank" 
+          >
+            <img 
+              src="/factura-ERN/assets/img/Binte-Logo.png" 
+              alt="logo empresa" 
+              className="w-24 ml-2"
+            />
+          </a>
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
