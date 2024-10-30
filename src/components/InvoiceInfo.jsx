b90f58f557030291e18f0b3560fe619d6d092c28
Jair <jairaceves56@gmail.com>
2024-10-30 16:34:17 -0600

    Datos agregados

--- src/components/InvoiceInfo.jsx
+++ src/components/InvoiceInfo.jsx
@@ -9,15 +9,15 @@ export const InvoiceInfo = () => {
         <div className="grid grid-cols-2 gap-4 text-gray-600">
             <div className="text-sm p-4">
                 <ul className='list-disc m-2'>
-                    <li>Para un mejor servicio, tienes 30 días naturales a partir de la fecha de tu compra para solicitar tu factura.</li>
                     <li>Recuerda que tienes hasta el último día del mes para generar tu factura.</li>
+                    <li>Recuerda que la fecha de la factura corresponde al dia que la generas.</li>
+                    <li>Ingresa el folio de tu ticket o nota de venta.</li>
                 </ul>
             </div>
             <div className="text-sm p-4">
                 <ul className='list-disc m-2'>
-                    <li>Ingresa el folio de tu ticket o nota de venta.</li>
                     <li>Revisa que tus datos fiscales sean correctos.</li>
-                    <p>(RFC, Razón social, Regimen Fiscal, Uso de CFDI)</p>
+                    <p>(RFC, Razón Social, Regimen Fiscal, Uso de CFDI)</p>
                     <li>Una vez timbrada la factura no podrá ser Cancelada.</li>
                 </ul>
             </div>
