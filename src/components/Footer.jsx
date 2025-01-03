d8e0c8fab3c9bf084fff682592e91bc45763a3e2
Jair <jairaceves56@gmail.com>
2025-01-03 13:22:30 -0600

    Descargar pdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export const Footer = () => {
               Aviso de Privacidad
             </a>
           </p>
-          <p className="text-gray-400">V. 1.0.3</p>
+          <p className="text-gray-400">V. 1.0.4</p>
         </div>
         <p className="text-white text-lg flex items-center">
           Desarrollado por:
