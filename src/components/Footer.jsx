7656b9bb40002b73d871145503acc12e83aa0ab0
Jair <jairaceves56@gmail.com>
2025-01-30 13:59:20 -0600

    Loader y Bloqueo del Boton agregado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export const Footer = () => {
               Aviso de Privacidad
             </a>
           </p>
-          <p className="text-gray-400">V. 1.0.9</p>
+          <p className="text-gray-400">V. 1.0.10</p>
         </div>
         <p className="text-white text-lg flex items-center">
           Desarrollado por:
